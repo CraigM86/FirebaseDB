dac3b194ea11e25f784d6464acc86917791c98c6
CraigM86 <craig@avenuetraining.com.au>
2020-11-18 18:34:49 +1000

    Initial Commit

---
+++ FirebaseDB/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FirebaseDB
+//
+//  Created by MAC on 18/11/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
